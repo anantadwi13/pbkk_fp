1ad212dc4969c69506d9a9b2de96542599792df3
ranggakd <ranggakusumad@gmail.com>
2020-04-26 23:41:36 +0700

    try route and controller signin
    belum selesai

---
+++ app/common/controllers/SignController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Common\Controllers;
+
+// use App\Utils\Sidebar\Item\Anchor;
+// use App\Utils\Sidebar\Item\SubMenu;
+// use App\Utils\Sidebar\Menu;
+use Phalcon\Mvc\Controller;
+
+class AppController extends Controller
+{
+    public function upAction()
+    {
+        $this->view->setLayout('up');
+    }
+    // public function initialize()
+    // {
+    //     $sidebar = new Menu();
+    //     $sidebar->addItem(new Anchor('Dashboard', $this->url->get('/'), 'si si-cup'))
+
+    //         ->addHeading('Collaboration', 'Co')
+    //         ->addItem(new Anchor('Sounds', $this->url->get('/sound'), 'si si-volume-2'))
+    //         ->addItem(new Anchor('Jadwal', $this->url->get('/calendar'), 'si si-calendar'))
+    //         ->addItem(new Anchor('Invitation', $this->url->get('/sound'), 'si si-envelope-letter'))
+
+    //         ->addHeading('Challenge', 'Ch')
+    //         ->addItem(
+    //             (new SubMenu('Submenu', 'si si-cup'))
+    //                 ->addItem(new Anchor('Item', '#'))
+    //         );
+
+    //     $this->setSideBar($sidebar);
+    // }
+
+    // /**
+    //  * @param Menu|null $sidebar
+    //  */
+    // public function setSideBar($sidebar)
+    // {
+    //     $this->view->sidebar = $sidebar;
+    // }
+}
