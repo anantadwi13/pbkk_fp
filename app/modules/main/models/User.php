5f0ae226b3028aca27beb46267090447d9d9c5e9
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2020-04-27 06:16:53 +0700

    fixed bug sqlsrv, add new models

--- app/modules/main/models/User.php
+++ app/modules/main/models/User.php
@@ -3,26 +3,29 @@
 namespace Dengarin\Main\Models;
 
 use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Resultset\Simple;
 
+/**
+ * Class User
+ * @property Simple|UserData    $data
+ * @method  Simple|UserData     getData($parameters = null)
+ * @method  integer             countData()
+ */
 class User extends Model
 {
     public $id;
-    public $name;
+    public $username;
     public $email;
+    public $nama;
+    public $password;
+    public $tipe;
+    public $status;
 
     public function initialize(){
-        $this->setSchema('dbo');
         $this->setSource('users');
-    }
-
-    public function onConstruct(){
-
-    }
-
-    public function toString(){
-        return
-            "ID : " . $this->id . ", " .
-            "Nama : " . $this->name . ", " .
-            "Email : " . $this->email . "";
+        $this->hasOne('id', UserData::class, 'id', [
+            'reusable' => true,
+            'alias' => 'data'
+        ]);
     }
 }
