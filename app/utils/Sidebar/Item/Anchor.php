8f85994aa6cce84d8e48d92e170df00941e061a2
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2020-04-28 14:20:03 +0700

    update sidebar utils

--- app/utils/Sidebar/Item/Anchor.php
+++ app/utils/Sidebar/Item/Anchor.php
@@ -4,14 +4,19 @@
 namespace App\Utils\Sidebar\Item;
 
 
-class Anchor
+use Phalcon\Di\Injectable;
+
+class Anchor extends Injectable
 {
     private $html = '';
+    public $active = false;
 
     public function __construct(string $name, string $href, $icon=null)
     {
+        $this->active = $this->request->getURI() == $href ? 'active' : '';
+
         $icon_html = !empty($icon) ? "<i class=\"$icon\"></i>" : "";
-        $this->html .= "<a href=\"$href\">$icon_html $name</a>";
+        $this->html .= "<a class='$this->active' href=\"$href\">$icon_html $name</a>";
     }
 
     public function generate(): string
